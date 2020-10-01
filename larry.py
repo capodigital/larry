33f283f14eca2c3cb3a162c005939a22f48b892a
Capo Digital <hola@capo.digital>
2020-09-30 23:38:08 -0300

    Larry first appear
    Larry will be my helper. He will make mundane tasks for me. He may be expensive, but his worth every penny.

--- larry.py
+++ larry.py
@@ -1,1 +1,28 @@
-import this
+import os
+
+class Larry():
+    def __init__(self):
+        """
+        This should have something, right?
+        """
+        
+    def rename(self, path, old, new):
+        try:
+            for file in os.listdir(path):
+                oldfile = os.path.join(path, file)
+                newfile = os.path.join(path, file.replace(old, new))
+                os.rename(oldfile, newfile)
+        except Exception as e:
+            self.exceptionHendler(e)
+
+    def exceptionHendler(self, exceptioncatch):
+        print("Even a pro has a his limits: {0}".format(exceptioncatch))
+
+if '__main__' == __name__:
+    larry = Larry()
+    path = r""
+    old = ""
+    new = ""
+    larry.rename(path, old, new)
+
+
